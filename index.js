5512dac1b84489cbf552e9d5b8b1e72725d71bc9
Alice <marja1895@gmail.com>
2020-10-19 11:00:17 +0300

    starting app

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const { ApolloServer } = require('appolo-server');
+const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
-
 const typeDefs = gql`
     type Query{
         sayHi: String!
-    }
+    };
 
 const resolvers = {
-    
-}
+    Query: {
+        sayHi: () => 'Hello World!'
+    }
+};
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+server.listen({ port: 3000 }).then(res => {
+    console.log(`Server running at ${res.url}`);
+})
